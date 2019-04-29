df82723d08b600e0b5aecb992c013fb788296a6e
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-29 16:31:33 +0300

    edit main view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Product/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Product from '@/components/ProductList.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Product
   }
 }
 </script>
