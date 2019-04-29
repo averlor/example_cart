e18bed5688429d86c2e11d9f98da6a68ba30a260
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-29 16:29:43 +0300

    added module and localStorage

--- src/store.js
+++ src/store.js
@@ -1,16 +1,26 @@
+// localStorage
+let cart = window.localStorage.getItem('cart');
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
 
+import shopCart from './store/cart.js'
+import books from './store/books.js'
+
+export default new Vuex.Store({
+  modules: {
+    shopCart,
+    books
+  },
+  state: {
+    cart : cart ? JSON.parse(cart) : [] 
   },
   mutations: {
-
-  },
-  actions: {
-
+    SAVE_CART(state) {
+      window.localStorage.setItem('cart', JSON.stringify(state.cart));
+    }
   }
 })
